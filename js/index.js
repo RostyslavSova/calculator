ca63813f9699ab477bc00c9f69e9b3e46c63ee15
Rostyslav Sova <sova.rostislav@gmail.com>
2018-03-04 18:58:31 +0000

    init

---
+++ js/index.js
@@ -0,0 +1,50 @@
+var keys = document.querySelectorAll('input[type="button"]');
+var operators = ['+', '-', 'x', '÷'];
+var decimalAdded = false;
+
+for(var i = 0; i < keys.length; i++) {
+	keys[i].onclick = function(e) {
+	
+		var input = document.querySelector('.screen');
+		var inputVal = input.value;
+		var btnVal = this.value;
+		
+		if(btnVal == 'C') {
+			input.value = '';
+			decimalAdded = false;
+		} else if(btnVal == '=') {
+			var equation = inputVal;
+			var lastChar = equation[equation.length - 1];
+			
+			equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
+			
+			if(operators.indexOf(lastChar) > -1 || lastChar == '.')
+				equation = equation.replace(/.$/, '');
+			
+			if(equation)
+				input.value = eval(equation);
+				
+			decimalAdded = false;
+		} else if(operators.indexOf(btnVal) > -1) {
+			var lastChar = inputVal[inputVal.length - 1];
+			if(inputVal != '' && operators.indexOf(lastChar) == -1) 
+				input.value += btnVal;
+			else if(inputVal == '' && btnVal == '-') 
+				input.value += btnVal;
+			if(operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
+				input.value = inputVal.replace(/.$/, btnVal);
+			}
+			
+			decimalAdded =false;
+		} else if(btnVal == '.') {
+			if(!decimalAdded) {
+				input.value += btnVal;
+				decimalAdded = true;
+			}
+		} else {
+			input.value += btnVal;
+		}
+		
+		e.preventDefault();
+	} 
+}
