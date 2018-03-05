a094e6964f3544943a4a0c307d2da2d312a80e6a
Rostyslav Sova <sova.rostislav@gmail.com>
2018-03-05 14:26:55 +0000

    initial

--- js/index.js
+++ js/index.js
@@ -1,50 +1,50 @@
 var keys = document.querySelectorAll('input[type="button"]');
-var operators = ['+', '-', 'x', '÷'];
+var operators = ['+', '-', '*', '/'];
 var decimalAdded = false;
 
-for(var i = 0; i < keys.length; i++) {
+for (var i = 0; i < keys.length; i++) {
 	keys[i].onclick = function(e) {
-	
-		var input = document.querySelector('.screen');
+
+		var input = document.querySelector('.answer');
 		var inputVal = input.value;
 		var btnVal = this.value;
-		
-		if(btnVal == 'C') {
+
+		if (btnVal == 'C') {
 			input.value = '';
 			decimalAdded = false;
-		} else if(btnVal == '=') {
+		} else if (btnVal == '=') {
 			var equation = inputVal;
 			var lastChar = equation[equation.length - 1];
-			
-			equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
-			
-			if(operators.indexOf(lastChar) > -1 || lastChar == '.')
+			if (operators.indexOf(lastChar) > -1 || lastChar == '.'){
 				equation = equation.replace(/.$/, '');
-			
-			if(equation)
+			} 
+			if (equation) {
 				input.value = eval(equation);
-				
+			}
+
 			decimalAdded = false;
-		} else if(operators.indexOf(btnVal) > -1) {
+		} else if (operators.indexOf(btnVal) > -1) {
 			var lastChar = inputVal[inputVal.length - 1];
-			if(inputVal != '' && operators.indexOf(lastChar) == -1) 
+			if (inputVal !== '' && operators.indexOf(lastChar) === -1) {
 				input.value += btnVal;
-			else if(inputVal == '' && btnVal == '-') 
+			} else if (inputVal == '' && btnVal == '-') {
 				input.value += btnVal;
-			if(operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
-				input.value = inputVal.replace(/.$/, btnVal);
 			}
 			
-			decimalAdded =false;
-		} else if(btnVal == '.') {
-			if(!decimalAdded) {
+			if (operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
+				input.value = inputVal.replace(/.$/, btnVal);
+			}
+
+			decimalAdded = false;
+		} else if (btnVal == '.') {
+			if (!decimalAdded) {
 				input.value += btnVal;
 				decimalAdded = true;
 			}
 		} else {
 			input.value += btnVal;
 		}
-		
-		e.preventDefault();
-	} 
+
+	
+	}
 }
