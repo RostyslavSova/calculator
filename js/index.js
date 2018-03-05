15e3516e84c3f7419fb1971e679e53f6f73002e0
Rostyslav Sova <sova.rostislav@gmail.com>
2018-03-05 14:38:25 +0000

    initial5

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ let operators = ['+', '-', '*', '/'];
  let decimalAdded = false;
 
 for (let i = 0; i < keys.length; i++) {
-	keys[i].onclick = function(e) {
+	keys[i].onclick = function() {
 
 		let input = document.querySelector('.answer');
 		let inputVal = input.value;
